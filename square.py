0b2aceeaa968a95223759b2bf9774da5ca7912e5
Marc Abramowitz <abramowi@adobe.com>
2017-04-14 16:04:49 -0700

    Add some files

---
+++ square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from miniSim import MiniSim
+
+miniSim = MiniSim()
+robot = miniSim.robot0
+
+
+def go():
+    miniSim.resetRobot(robot)
+    robot.penWidth = 30
+    robot.penDown()
+
+    for _i in range(4):
+        robot.move(200)
+        robot.rotate(90)
+
+
+miniSim.go = go
+miniSim.run()
