87a8a1da70fd88e208c948a24d439d6ef0e531c3
Marc Abramowitz <abramowi@adobe.com>
2017-04-14 17:03:10 -0700

    Make sure robot0.png packaged & installed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8 :
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = '0.0'
 long_description_filename = os.path.join(
@@ -15,10 +15,12 @@ setup(
     author="Julián U. da Silva Gillig",
     url="https://github.com/msabramo/miniSim",
     keywords='robotics simulation educational python',
-    description="Robot simulation forked from miniBloq at https://github.com/miniBloq/v0.83",
+    description="Robot simulation forked from miniBloq" +
+                "at https://github.com/miniBloq/v0.83",
     long_description=long_description,
     license='RobotGroup – Multiplo Pacifist License (RMPL) 1.0',
     install_requires=['pygame'],
-    py_modules=['miniSim'],
+    packages=find_packages(),
+    include_package_data=True,
     zip_safe=False,
 )
